c549f811ce710da517c602a890419f088cb003d6
Tyler Wilson <tyler.wilson.0407@gmail.com>
2019-02-22 16:08:34 -0600

    Got triangle function working and tested by rendering model head.  Next will clean up code and then work on shading.

--- drawing.cpp
+++ drawing.cpp
@@ -4,16 +4,55 @@
 #include "drawing.h"
 #include "tgaimage.h"
 
+// structures
+
 // utility functions
 
-int cross2I(Point& v0, Point& v1) {
-    /* cross product of 2D vectors with integer components.  Resulting vector 
+int cross2I(const Vec2I& v0, const Vec2I& v1) {
+    /* Cross product of 2D vectors with integer components.  Resulting vector 
  * points in z-direction.  Vectors represented as Points(vec = 0 -> point) */
     
     int k = (v0.x * v1.y) - (v0.y * v1.x);
     
     return k;
 }
+
+int sign(const int& x) {
+    /* Returns sign of int.  (x>=0) returns 1.  (x<0) returns -1.*/
+    return (x >= 0) - (x < 0);
+}
+
+bool inside_triangle(const Vec2I* pts, const Vec2I& P) {
+    /* Returns true if points P is inside triangle of points in pts*/
+    
+    // pre-compute values that are used multiple times
+    int y1_y2 = pts[1].y - pts[2].y;
+    int x2_x1 = pts[2].x - pts[1].x;
+    int x0_x2 = pts[0].x - pts[2].x;
+    int y0_y2 = pts[0].y - pts[2].y;
+    int y2_y0 = pts[2].y - pts[0].y;
+    
+    int det = (y1_y2 * x0_x2) + (x2_x1 * y0_y2);
+    int sign_det = sign(det);
+    
+    int lambda_1 = sign_det * ((y1_y2 * (P.x - pts[2].x)) + (x2_x1 * (P.y - pts[2].y)));
+    if (lambda_1 >= 0) {
+//        std::cout << "lambda1: " << lambda_1 << std::endl;
+        int lambda_2  = sign_det * ((y2_y0 * (P.x - pts[2].x)) + (x0_x2 * (P.y - pts[2].y)));
+        if (lambda_2 >= 0) {
+//            std::cout << "lambda2: " << lambda_2 << std::endl;
+            int lambda_3 = det - sign_det * (lambda_1 + lambda_2);
+            if ((sign_det>0) ? (lambda_3 > 0) : (lambda_3 < 0)) {
+//                std::cout << "lambda3: " << lambda_3 << std::endl;
+                return true;
+            }   
+        }
+    }
+    
+    return false;
+}
+
+
 
 // line drawing functions
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -115,10 +154,10 @@ void vert_line(int x, int y0, int y1, TGAImage &image, TGAColor color) {
 }
 
 // triangle drawing functions
-void triangle (Point A, Point B, Point C, TGAImage &image, TGAColor color) {
+void triangle (Vec2I A, Vec2I B, Vec2I C, TGAImage &image, TGAColor color) {
     
     // add points into array
-    Point pts[] = {A, B, C};
+    Vec2I pts[] = {A, B, C};
     
     /* get upper and lower bounds of x and y to create bounding box*/
     // initialize with x/y of point A
@@ -127,7 +166,7 @@ void triangle (Point A, Point B, Point C, TGAImage &image, TGAColor color) {
     
     // loop through B and C looking for lesser/greater values of x/y
     // REWRITE TO LOOP THROUGH {B, C}, looks cleaner!!!
-    for (Point* ptr = pts + 1; ptr <= pts + 2; ptr++) {
+    for (Vec2I* ptr = pts + 1; ptr <= pts + 2; ptr++) {
         
         if (ptr->x < x_bound[0]) {
             x_bound[0] = ptr->x;
@@ -145,46 +184,32 @@ void triangle (Point A, Point B, Point C, TGAImage &image, TGAColor color) {
     std::cout << x_bound[0] << ", " << x_bound[1] << std::endl;
     std::cout << y_bound[0] << ", " << y_bound[1] << std::endl;
     
-    /* make vectors(represented as Points) for AB and AC and put into array*/
-    Point vecs[2];
-    for (int i = 0; i < 2; i++) {
-        vecs[i] = pts[i + 1] - pts[0];
-    }
-    
-    // use instead of vecs array??
-    Point AB = B - A;
-    Point AC = C - A;
-    
-    int ABxAC = -cross2I(vecs[0], vecs[1]);
-    std::cout << "ABxAC:" << ABxAC << std::endl;
-    
     for (int x = x_bound[0]; x < x_bound[1]; x++) {
         for (int y = y_bound[0]; y < y_bound[1]; y++) {
             
-            Point P(x, y);
-            Point AP = P - A;
+            Vec2I P(x, y);
             
-            /* Calculate cross products here(note that multiplied thru by ABxAC
-             * as well).  Figure out best way to check for negative at each calc
-             * and go to next iteration if so*/
-            
-            // u value
-            int ACxAP = cross2I(AC, AP);
-            if (ACxAP > 0) {
-//                std::cout << "u:" << ACxAP << std::endl;
-                // v value
-                int ABxAP = cross2I(AB, AP);
-                if (ABxAP > 0) {
-//                    std::cout << "v:" << ABxAP << std::endl;
-                    // w value
-                    int w = ABxAC - ACxAP - ABxAP;
-                    if (w > 0) {
-//                        std::cout << "w:" << w << std::endl;
-                        image.set(x, y, color);
-                    }
-                }
+            if (inside_triangle(pts, P)) {
+                image.set(x, y, color);
             }
+        }
+    }
+    
+    return;
+}
+
+void triangle (Vec2I* pts, TGAImage &image, TGAColor color) {
+
+    BoundingBox bbox(pts);
+    
+    for (int x = bbox.x_lower; x < bbox.x_upper; x++) {
+        for (int y = bbox.y_lower; y < bbox.y_upper; y++) {
             
+            Vec2I P(x, y);
+            
+            if (inside_triangle(pts, P)) {
+                image.set(x, y, color);
+            }
         }
     }
     
